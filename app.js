a00a92da80f2dfcf257d5da1450e47af3c9e165a
Kailey Colestock <kaileycolestock@Kaileys-MacBook-Pro.local>
2016-02-18 12:11:57 -0600

    Corrected people/PEorpl typos and almost complete assignment

--- app.js
+++ app.js
@@ -3,21 +3,148 @@ var app = express();
 var bodyParser = require('body-parser');
 var path = require('path');
 //var data = require('./routes/data');
-var name = require('./routes/name');
-var animals = require('./routes/animals');
-var assigner = require('./routes/assigner');
+//var name = require('./routes/name');
+//var animals = require('./routes/animals');
+//var assigner = require('./routes/assigner');
 
+var pg = require('pg');
+var connectionString = '';
+if(process.env.DATABASE_URL != undefined) {
+    connectionString = process.env.DATABASE_URL + 'ssl';
+} else {
+    connectionString = 'postgres://localhost:5432/spirit_animal_assigner';
+}
+
+
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/people', function(req, res) {
+    var results = [];
+    pg.connect(connectionString, function(err, client, done) {
+        var query = client.query('SELECT people.first_name, people.last_name, people.person_id FROM people ORDER BY' +
+            ' person_id ASC;');
+        //  JOIN animal ON people.animal_id = animal.animal_id
+        // Stream results back one row at a time
+        query.on('row', function(row) {
+            results.push(row);
+        });
+
+        // close connection
+        query.on('end', function() {
+            console.log('this is the results: ' + results[0]);
+            return res.json(results);
+            client.end(); //because we're doing .end here we don't need to do done
+
+        });
+        if(err) {
+            console.log(err);
+        }
+    });
+});
+
+app.get('/animal', function(req, res) {
+    var results = [];
+    pg.connect(connectionString, function(err, client, done) {
+        var query = client.query('SELECT animal.animal_name, animal.animal_color, animal.animal_id FROM animal ORDER' +
+            ' BY animal_id ASC;');
+        query.on('row', function(row) {
+            results.push(row);
+        });
+
+        query.on('end', function() {
+            console.log('this is the animal results: ' + results[0]);
+            return res.json(results);
+            client.end();
+
+        });
+        if(err) {
+            console.log(err);
+        }
+    });
+});
+
+
+app.post('/people', function(req, res) {
+    var addPerson = {
+        firstName: req.body['first-name'],
+        lastName: req.body['last-name']
+
+    };
+
+    pg.connect(connectionString, function(err, client, done) {
+        client.query("INSERT INTO people (first_name, last_name) VALUES ($1, $2)",
+            [addPerson.firstName, addPerson.lastName],
+            function (err, result) {
+                done(); //done closes the connection to the database
+                if(err) {
+                    console.log("Error inserting data: ", err);
+                    res.send(false);
+                } else {
+                    res.send(result);
+                }
+            });
+    });
+
+});
+
+
+app.post('/animal', function(req, res) {
+    var addAnimal = {
+        animal: req.body['spirit-animal'],
+        animalColor: req.body['spirit-animal-color']
+    };
+
+    console.log('addAnimal object' + addAnimal);
+
+    pg.connect(connectionString, function(err, client, done) {
+        client.query("INSERT INTO animal (animal_name, animal_color) VALUES ($1, $2)",
+            [addAnimal.animal, addAnimal.animalColor],
+            function (err, result) {
+                done(); //done closes the connection to the database
+                if(err) {
+                    console.log("Error inserting data: ", err);
+                    res.send(false);
+                } else {
+                    res.send(result);
+                }
+            });
+    });
+
+});
+
+app.post('/assign', function(req, res) {
+    var assAnimal = {
+        animalID: req.body['animal-id-assignment'],
+        personID: req.body['personID']
+    };
+
+    pg.connect(connectionString, function(err, client, done) {
+        client.query("UPDATE people SET animal_id = $1 WHERE person_id = $2",
+            [assAnimal.animalID, assAnimal.personID],
+            function (err, result) {
+                done(); //done closes the connection to the database
+                if(err) {
+                    console.log("Error inserting data: ", err);
+                    res.send(false);
+                } else {
+                    res.send(result);
+                }
+            });
+    });
+
+});
+
 app.set('port', process.env.PORT || 5000);
 //app.use('/data', data);
-app.use('/name', name.name); //2.go to name.name
-app.use('/animals', animals.animals);
-//app.use('/assigner', assigner);
-
-app.get('/assigner', function(req, res){
-    console.log('in assigner route');
-    res.send(assigner());
-});
+//app.use('/name', name.name); //2.go to name.name
+//app.use('/animals', animals.animals);
+////app.use('/assigner', assigner);
+//
+//app.get('/assigner', function(req, res){
+//    console.log('in assigner route');
+//    res.send(assigner());
+//});
 
 app.get('/*', function(req,res) {
     console.log('Here is the request: ', req.params);
--- public/scripts/clientapp.js
+++ public/scripts/clientapp.js
@@ -1,73 +1,254 @@
-var animalsArray = [];
-var nameArray = [];
-
-$(document).ready(function(){
-    //clickPostData();
-    pageReady('name');
-    pageReady('animals');
-    $('#post-data').on('click', clickPostData);
-    //$('#get-data').on('click', getData);
-    $('#assign').on('click', assignAnimals);
+$(document).ready(function() {
+    getData();
+    getAnimalData();
+    $('#post-people').on('click', postPeople);
+    $('#post-animals').on('click', postAnimals);
+    $('#name-list').on('click', '.assign-animal', assigningAnimals);
 });
 
-function pageReady(fileName) {
-    //On refresh page doesn't display lists, fix this without having empty array spots
-    $.ajax({
-        type: 'GET',
-        url: '/' + fileName, //1.go to node server (app.js)
-        success: function(data) {
-            //array = [];
-            //$('#' + fileName + '-list').empty();
-            console.log(data);
-            for(var i = 0; i < data.length; i++) {
-                $('#' + fileName + '-list').append('<p>' + data[i] + '</p>');
-            }
-        }
-    });
-}
 
-function clickPostData() {
+
+function postPeople() {
     event.preventDefault();
-    postData('#name-form', 'name', nameArray); //This doesn't watch for empty strings and records them in the array
-    postData('#animal-form', 'animals', animalsArray); //even if an input field is empty
-}
-
-function postData (form, file, array) {
     var values = {};
-    $.each($(form).serializeArray(), function(i, field) {
+
+    $.each($('#name-form').serializeArray(), function(i, field) {
         values[field.name] = field.value;
     });
-    $(form).find('input[type=text]').val('');
-    //console.log(values);
+
+    console.log(values);
+
     $.ajax({
         type: 'POST',
-        url: '/' + file,
+        url: '/people',
         data: values,
         success: function(data) {
-            console.log('From Server: ', data);
-            $('#' + file + '-list').empty(); //consider declaring a variable for $('#' + file + '-list')
-            //array.empty();
-            //array.push(data);
-            array = [];
-            for(var i = 0; i < data.length; i++) {
-                $('#' + file + '-list').append('<p>' + data[i] + '</p>');
-                array.push(data[i]);
-                //$(file + 'Array').push(data[i]);
-                //console.log(file + 'Array');
+            if(data) {
+                // everything went ok
+                console.log('from server:', data);
+                getData();
+            } else {
+                console.log('error');
             }
-            console.log('Here is the ' + file + ' array: ' + array);
         }
     });
-    //console.log('Here is the ' + file + ' array: ' + array);
+
+
 }
 
-function assignAnimals() {
+function postAnimals() {
+    event.preventDefault();
+    var values = {};
+
+    $.each($('#animal-form').serializeArray(), function(i, field) {
+        values[field.name] = field.value;
+    });
+
+    console.log('aminal values ' + values);
+
+    $.ajax({
+        type: 'POST',
+        url: '/animal',
+        data: values,
+        success: function(data) {
+            if(data) {
+                // everything went ok
+                console.log('from aminal server:', data);
+                getAnimalData();
+            } else {
+                console.log('error');
+            }
+        }
+    });
+
+
+}
+
+function getData() {
     $.ajax({
         type: 'GET',
-        url: '/assigner',
+        url: '/people',
         success: function(data) {
-            $('#spirit-animal-assignments').append('<p>' + data + '</p>');
-            console.log(data);
+            appendPeople(data);
+            console.log('Hellllo data!! ' + data);
         }
     });
 }
+
+function getAnimalData() {
+    $.ajax({
+        type: 'GET',
+        url: '/animal',
+        success: function(data) {
+            appendAnimals(data);
+            console.log('Hellllo data!! ' + data);
+        }
+    });
+}
+
+function appendPeople(info) {
+    $('#name-list').empty();
+    for (var i = 0; i < info.length; i++) {
+        $('#name-list').append('<div></div>');
+        var $el = $('#name-list').children().last();
+        $el.append('<p>' + info[i].first_name + ' ' + info[i].last_name + '</p>'); //form
+        $el.append('<form id="' + info[i].person_id + '"><label for="animal-id-assignment">AMINAL ID: </label><input' +
+            ' id="animal-id-assignment" type="number" name="animal-id-assignment"></input></form>');
+        $el.append('<button class="assign-animal">Assign AMINALS</button>');
+        console.log(info[i]);
+        console.log(info[i].person_id);
+
+    }
+}
+
+function appendAnimals(info) {
+    $('#animals-list').empty();
+    for (var i = 0; i < info.length; i++) {
+        $('#animals-list').append('<div></div>');
+        var $el = $('#animals-list').children().last();
+        $el.append('<p>' + info[i].animal_name + ' ' + info[i].animal_color + '</p>');
+    }
+}
+
+
+
+//create event listener for .assign-animal class button!!!!!!!!!!!!!!!!!!1
+
+function assigningAnimals() { //AMINALS are being assigned on the DB but not yet appending to DOM :P
+    event.preventDefault();
+
+    var id = $(this).siblings('form').attr('id');
+    console.log(id);
+    var $$$el = $(this).parent().children('p');
+    var values = {};
+
+    $.each($('#' + id).serializeArray(), function(i, field) {
+        values[field.name] = field.value;
+    });
+
+    values.personID = id;
+
+    console.log(values);
+
+    $.ajax({
+        type: 'POST',
+        url: '/assign',
+        data: values,
+        beforeSend: console.log(values),
+        success: function(data) {
+            if(data) {
+                // everything went ok
+                console.log('from server:', data);
+                //getData();
+            } else {
+                console.log('error');
+            }
+        }
+    });
+
+    $('#spirit-animal-assignments').append($$$el);
+    $('#spirit-animal-assignments').append('<p>You know what you are...</p>');
+    $(this).parent().remove();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//var animalsArray = [];
+//var nameArray = [];
+//
+//$(document).ready(function(){
+//    //clickPostData();
+//    pageReady('name');
+//    pageReady('animals');
+//    $('#post-data').on('click', clickPostData);
+//    //$('#get-data').on('click', getData);
+//    $('#assign').on('click', assignAnimals);
+//});
+//
+//function pageReady(fileName) {
+//    //On refresh page doesn't display lists, fix this without having empty array spots
+//    $.ajax({
+//        type: 'GET',
+//        url: '/' + fileName, //1.go to node server (app.js)
+//        success: function(data) {
+//            //array = [];
+//            //$('#' + fileName + '-list').empty();
+//            console.log(data);
+//            for(var i = 0; i < data.length; i++) {
+//                $('#' + fileName + '-list').append('<p>' + data[i] + '</p>');
+//            }
+//        }
+//    });
+//}
+//
+//function clickPostData() {
+//    event.preventDefault();
+//    postData('#name-form', 'name', nameArray); //This doesn't watch for empty strings and records them in the array
+//    postData('#animal-form', 'animals', animalsArray); //even if an input field is empty
+//}
+//
+//function postData (form, file, array) {
+//    var values = {};
+//    $.each($(form).serializeArray(), function(i, field) {
+//        values[field.name] = field.value;
+//    });
+//    $(form).find('input[type=text]').val('');
+//    //console.log(values);
+//    $.ajax({
+//        type: 'POST',
+//        url: '/' + file,
+//        data: values,
+//        success: function(data) {
+//            console.log('From Server: ', data);
+//            $('#' + file + '-list').empty(); //consider declaring a variable for $('#' + file + '-list')
+//            //array.empty();
+//            //array.push(data);
+//            array = [];
+//            for(var i = 0; i < data.length; i++) {
+//                $('#' + file + '-list').append('<p>' + data[i] + '</p>');
+//                array.push(data[i]);
+//                //$(file + 'Array').push(data[i]);
+//                //console.log(file + 'Array');
+//            }
+//            console.log('Here is the ' + file + ' array: ' + array);
+//        }
+//    });
+//    //console.log('Here is the ' + file + ' array: ' + array);
+//}
+////
+////function assignAnimals() {
+////    $.ajax({
+////        type: 'GET',
+////        url: '/assigner',
+////        success: function(data) {
+////            $('#spirit-animal-assignments').append('<p>' + data + '</p>');
+////            console.log(data);
+////        }
+////    });
+////}
